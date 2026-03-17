4d8392592fcc438ab34fa97772991328ddbfa632
souper GRRR MARICE <soraya.ahmadi@student.tersterre.be>
2026-03-17 09:47:15 +0100

    function to delete character n name fixes

--- script.js
+++ script.js
@@ -1,97 +1,118 @@
 // JavaScript Document
 //onClick voor de dingste
-const characters =[{
+let characters =[{
 
 	id: 1,
 	power: 50,
 	name: "Eline",
-	roles: "leerkracht",
+	role: "leerkracht",
 	
 	}, {
 	
 	id: 2,
 	power: 1000000000000,
 	name: "ferre",
-	roles: "zwerver",
+	role: "zwerver",
 	}, {
 
 	id: 3,
 	power: 67,
 	name: "vinn",
-	roles: "turk",
+	role: "turk",
 	
 	}, {
 
 	id: 4,
 	power: 4,
 	name: "maurice",
-	roles: "koffie",
+	role: "koffie",
 	
-	},
+	}
 
 
 ];
 
 console.log(characters);
 function displayCharactersList() {
-	const container = document.getElementById("characters-list");
-	const divInfoContainer = document.createElement("div");
-	divInfoContainer.className = "info-container";
-	for(var i=0; i<characters.length; i++){
-		const pName = document.createElement("p");
-		pName.className = "name";
-		const spanNameLabel = document.createElement("span");
-		spanNameLabel.textContent = "Name: ";
-		const spanNameValue = document.createElement("span");
-		spanNameValue.className = "name-value";
-		spanNameValue.textContent = characters[i].name;
-		pName.appendChild(spanNameLabel);
-		pName.appendChild(spanNameValue);
-		divInfoContainer.appendChild(pName);
-		
-		
-		const pPower = document.createElement("p");
-		pPower.className = "power";
-		const spanPowerLabel = document.createElement("span");
-		spanPowerLabel.textContent = "Power: ";
-		const spanPowerValue = document.createElement("span");
-		spanPowerValue.className = "power-value";
-		spanPowerValue.textContent = characters[i].power;
-		pPower.appendChild(spanPowerLabel);
-		pPower.appendChild(spanPowerValue);
-		divInfoContainer.appendChild(pPower);
-		
-		const pRole = document.createElement("p");
-		pPower.className = "role";
-		const spanRoleLabel = document.createElement("span");
-		spanRoleLabel.textContent = "role: ";
-		const spanRoleValue = document.createElement("span");
-		spanRoleValue.className = "role-value";
-		spanRoleValue.textContent = characters[i].role;
-		pRole.appendChild(spanRoleLabel);
-		pRole.appendChild(spanRoleValue);
-		divInfoContainer.appendChild(pRole);
-		
-		const pButton = document.createElement("p");
-		pButton.className = "button";
-		const spanButtonLabel = document.createElement("span");
-		spanButtonLabel.textContent = "X";
-		spanButtonLabel.addEventListener("onClick",deleteCharacter(characters[i].id))
+	const container = document.getElementById("characters-container");
+	container.innerHTML = "";
 	
-		pRole.appendChild(spanButtonLabel);
-		divInfoContainer.appendChild(pButton);
-		
-		container.appendChild(divInfoContainer);
-			
+	for(var i=0; i<characters.length; i++){
+		createCharacterCard(characters[i]);
 
 	}
+}
+
+function createCharacterCard(character) {
+	const container = document.getElementById("characters-container");
+	
+	const characterContainer = document.createElement("div");
+	characterContainer.classList = "col-6 col-m-12";
+
+
+	const divInfoContainer = document.createElement("div");
+	divInfoContainer.className = "info-container";
+	const pName = document.createElement("p");
+	pName.className = "name";
+	const spanNameLabel = document.createElement("span");
+	spanNameLabel.textContent = "Name: ";
+	const spanNameValue = document.createElement("span");
+	spanNameValue.className = "name-value";
+	spanNameValue.textContent = character.name;
+	pName.appendChild(spanNameLabel);
+	pName.appendChild(spanNameValue);
+	divInfoContainer.appendChild(pName);
+
+
+	const pPower = document.createElement("p");
+	pPower.className = "power";
+	const spanPowerLabel = document.createElement("span");
+	spanPowerLabel.textContent = "Power: ";
+	const spanPowerValue = document.createElement("span");
+	spanPowerValue.className = "power-value";
+	spanPowerValue.textContent = character.power;
+	pPower.appendChild(spanPowerLabel);
+	pPower.appendChild(spanPowerValue);
+	divInfoContainer.appendChild(pPower);
+
+	const pRole = document.createElement("p");
+	pRole.className = "role";
+	const spanRoleLabel = document.createElement("span");
+	spanRoleLabel.textContent = "role: ";
+	const spanRoleValue = document.createElement("span");
+	spanRoleValue.className = "role-value";
+	spanRoleValue.textContent = character.role;
+	pRole.appendChild(spanRoleLabel);
+	pRole.appendChild(spanRoleValue);
+	divInfoContainer.appendChild(pRole);
+
+	const pButton = document.createElement("p");
+	pButton.className = "button";
+	const spanButtonLabel = document.createElement("span");
+	spanButtonLabel.textContent = "X";
+	spanButtonLabel.addEventListener("click", () => deleteCharacter(character));
+
+	pButton.appendChild(spanButtonLabel);
+	divInfoContainer.appendChild(pButton);
+
+	characterContainer.appendChild(divInfoContainer);
+
+	const imgContainer = document.createElement("div");
+	imgContainer.className="img-container";
+
+	characterContainer.appendChild(imgContainer);
+
+	container.appendChild(characterContainer);
+	
+}
 document.addEventListener("DOMContentLoaded", function(){
 	displayCharactersList();
-})
+});
 
-	
-function deleteCharacter(index){
-	
+
+function deleteCharacter(character){
+	characters = characters.filter(item => item !== character);
+	displayCharactersList();
 }
 	
 	
