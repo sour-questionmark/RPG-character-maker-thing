a06bf50ab40c5706c4fa7ecf23fecb5bb305a6b3
boris honck <162339176+mauricedonck@users.noreply.github.com>
2026-04-21 09:48:23 +0200

    cleared form on submit

--- script.js
+++ script.js
@@ -1,55 +1,19 @@
 // JavaScript Document
 //onClick voor de dingste
-let characters =[{
-
-	id: 1,
-	power: 50,
-	name: "soraya",
-	role: "yuri enjoyer",
-	
-	}, {
-	
-	id: 2,
-	power: 1000000000000,
-	name: "ferre",
-	role: "zwerver",
-	}, {
-
-	id: 3,
-	power: 67,
-	name: "vinn",
-	role: "turk",
-	
-	}, {
-
-	id: 4,
-	power: 4,
-	name: "maurice",
-	role: "koffie",
-	
-	}
-
-
+let characters =[
 ];
 
-console.log(characters);
 function displayCharactersList() {
 	const container = document.getElementById("characters-container");
 	container.innerHTML = "";
-	
 	for(var i=0; i<characters.length; i++){
 		createCharacterCard(characters[i]);
-
 	}
 }
-
 function createCharacterCard(character) {
 	const container = document.getElementById("characters-container");
-	
 	const characterContainer = document.createElement("div");
 	characterContainer.classList = "col-6 col-m-12";
-
-
 	const divInfoContainer = document.createElement("div");
 	divInfoContainer.className = "info-container";
 	const pName = document.createElement("p");
@@ -62,8 +26,6 @@ function createCharacterCard(character) {
 	pName.appendChild(spanNameLabel);
 	pName.appendChild(spanNameValue);
 	divInfoContainer.appendChild(pName);
-
-
 	const pPower = document.createElement("p");
 	pPower.className = "power";
 	const spanPowerLabel = document.createElement("span");
@@ -74,7 +36,6 @@ function createCharacterCard(character) {
 	pPower.appendChild(spanPowerLabel);
 	pPower.appendChild(spanPowerValue);
 	divInfoContainer.appendChild(pPower);
-
 	const pRole = document.createElement("p");
 	pRole.className = "role";
 	const spanRoleLabel = document.createElement("span");
@@ -85,37 +46,29 @@ function createCharacterCard(character) {
 	pRole.appendChild(spanRoleLabel);
 	pRole.appendChild(spanRoleValue);
 	divInfoContainer.appendChild(pRole);
-
+	const imgContainer = document.createElement("div");
+	imgContainer.className="img-container";
+	const img=document.createElement("img");
+	img.src=character.picture;
+	imgContainer.appendChild(img);
+	divInfoContainer.appendChild(imgContainer);
 	const pButton = document.createElement("p");
 	pButton.className = "button";
 	const spanButtonLabel = document.createElement("span");
 	spanButtonLabel.textContent = "X";
 	spanButtonLabel.addEventListener("click", () => deleteCharacter(character));
-
 	pButton.appendChild(spanButtonLabel);
 	divInfoContainer.appendChild(pButton);
-
 	characterContainer.appendChild(divInfoContainer);
-
-	const imgContainer = document.createElement("div");
-	imgContainer.className="img-container";
-
-	characterContainer.appendChild(imgContainer);
-
 	container.appendChild(characterContainer);
-	
 }
 document.addEventListener("DOMContentLoaded", function(){
 	displayCharactersList();
 });
-
-
 function deleteCharacter(character){
 	characters = characters.filter(item => item !== character);
 	displayCharactersList();
 }
-	
-
 function showAddCharacterContainer() {
 	document.getElementById("add-character-container").style.display = "block";
 	const fileInput = document.getElementById("add-character-picture-input");
@@ -124,27 +77,29 @@ function showAddCharacterContainer() {
 function previewFile() {
 	const file = document.getElementById("add-character-picture-input").files[0];
 	const reader = new FileReader();
-
 	reader.addEventListener("load", () => {
-		// convert image file to base64 string
 		document.getElementById("add-character-picture").src = reader.result;
 	});
 	if (file) {
 		reader.readAsDataURL(file);
 	}
 }
-
-
 function saveCharacter(){
 	const name = document.getElementById("add-character-name").value;
 	const power = document.getElementById("add-character-power").value;
 	const role = document.getElementById("add-character-role").value;
 	const picture = document.getElementById("add-character-picture").src;
-	
 	characters.push({
 		name: name,
-		role: "",
-		power: "",
+		role: role,
+		power: power,
 		picture: picture
-	})
+	});
+	displayCharactersList();
+	document.getElementById("add-character-name").value="";
+	document.getElementById("add-character-power").value="";
+	document.getElementById("add-character-role").value="";
+	document.getElementById("add-character-picture-input").value = "";
+	document.getElementById("add-character-picture").src="";
 }
+ 
